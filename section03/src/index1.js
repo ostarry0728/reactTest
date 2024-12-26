c737e70da488b96f874acfe777cf12dbaa653687
Oh SeungTaek <ostarry@gmail.com>
2024-12-26 16:10:09 +0900

    node js 기능 테스트

---
+++ section03/src/index1.js
@@ -0,0 +1,6 @@
+//외부에 있는 모듈을 가져옴.
+// const moudleData = require('./math');
+// const {add, sub} = require('./math');
+import {add, sub} from './math.js';
+console.log(add(20,20));
+console.log(sub(20,20));
